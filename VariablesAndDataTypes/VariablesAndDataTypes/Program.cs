f68a46da3b310e6b1e265ee1056cffc47fa2a3bc
rogerappt <marius.preda85@gmail.com>
2023-02-21 16:23:32 +0000

    Daily Report Assignment Submission

---
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is:" + yourName);
+            //Console.ReadLine();
+
+            //console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char randomTemperature = -23;
+            char questonMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.30202092;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Jesse";
+
+            int curentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+
+
+        }
+    }
+}
