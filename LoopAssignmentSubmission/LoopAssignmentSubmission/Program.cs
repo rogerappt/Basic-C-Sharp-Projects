e7b523b340b07bd004b90a1bd0e6628ce6ac4033
rogerappt <marius.preda85@gmail.com>
2023-02-22 00:04:27 +0000

    Loop Assignment Submission

---
+++ LoopAssignmentSubmission/LoopAssignmentSubmission/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LoopAssignmentSubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Example using a while statement
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine("While loop iteration: {0}", i);
+                i++;
+            }
+
+            // Example using a do while statement
+            int j = 0;
+            do
+            {
+                Console.WriteLine("Do while loop iteration: {0}", j);
+                j++;
+            } while (j < 5);
+
+            Console.ReadLine();
+        }
+    }
+}
