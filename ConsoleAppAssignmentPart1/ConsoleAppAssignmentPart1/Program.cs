e0534d40d01e6889dfe7a7c63c55672a8e8c6ba1
rogerappt <marius.preda85@gmail.com>
2023-02-22 17:04:06 +0000

    Console App Assignment Part 1

---
+++ ConsoleAppAssignmentPart1/ConsoleAppAssignmentPart1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleAppAssignmentPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "apple", "banana", "cherry" };
+            Console.Write("Enter some text: ");
+            string userInput = Console.ReadLine();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] += userInput;
+            }
+
+            Console.WriteLine("Resulting strings:");
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
