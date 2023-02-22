9c3e63dbc47e939f61c2fbfc06e1d36842a802c4
rogerappt <marius.preda85@gmail.com>
2023-02-22 23:25:47 +0000

    Method Assignment

---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MethodAssignment
+{
+    class MathOperations
+    {
+        public int Add(int num)
+        {
+            return num + 5;
+        }
+
+        public int Subtract(int num)
+        {
+            return num - 3;
+        }
+
+        public int Multiply(int num)
+        {
+            return num * 2;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to perform math operations on:");
+            int num = int.Parse(Console.ReadLine());
+
+            MathOperations mathOps = new MathOperations();
+
+            int result1 = mathOps.Add(num);
+            Console.WriteLine($"Result of adding 5 to {num} is {result1}");
+
+            int result2 = mathOps.Subtract(num);
+            Console.WriteLine($"Result of subtracting 3 from {num} is {result2}");
+
+            int result3 = mathOps.Multiply(num);
+            Console.WriteLine($"Result of multiplying {num} by 2 is {result3}");
+
+            Console.ReadLine();
+        }
+    }
+}
