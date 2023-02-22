ff4498ed7887f52876d4d908256bc4c77a1f36c0
rogerappt <marius.preda85@gmail.com>
2023-02-22 16:09:51 +0000

    String Assignment Submission 2

---
+++ ArraysAssignment/ArraysAssignment/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ArraysAssignment
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1: One-dimensional array of strings
+            string[] stringArray = { "apple", "banana", "cherry", "date" };
+            Console.Write("Enter an index for the string array (0-3): ");
+            int stringIndex = int.Parse(Console.ReadLine());
+            if (stringIndex >= 0 && stringIndex < stringArray.Length)
+            {
+                Console.WriteLine("The string at index {0} is {1}", stringIndex, stringArray[stringIndex]);
+            }
+            else
+            {
+                Console.WriteLine("The index {0} is out of range for the string array", stringIndex);
+            }
+
+            // Part 2: One-dimensional array of integers
+            int[] intArray = { 10, 20, 30, 40 };
+            Console.Write("Enter an index for the integer array (0-3): ");
+            int intIndex = int.Parse(Console.ReadLine());
+            if (intIndex >= 0 && intIndex < intArray.Length)
+            {
+                Console.WriteLine("The integer at index {0} is {1}", intIndex, intArray[intIndex]);
+            }
+            else
+            {
+                Console.WriteLine("The index {0} is out of range for the integer array", intIndex);
+            }
+
+            // Part 3: Display message when index is out of range
+            List<string> stringList = new List<string> { "Alice", "Bob", "Charlie", "Dave" };
+            Console.Write("Enter an index for the string list (0-3): ");
+            int listIndex = int.Parse(Console.ReadLine());
+            if (listIndex >= 0 && listIndex < stringList.Count)
+            {
+                Console.WriteLine("The string at index {0} is {1}", listIndex, stringList[listIndex]);
+            }
+            else
+            {
+                Console.WriteLine("The index {0} is out of range for the string list", listIndex);
+            }
+
+            // Part 4: List of strings
+            Console.Write("Enter an index for the string list (0-3): ");
+            int listIndex2 = int.Parse(Console.ReadLine());
+            if (listIndex2 >= 0 && listIndex2 < stringList.Count)
+            {
+                Console.WriteLine("The string at index {0} is {1}", listIndex2, stringList[listIndex2]);
+            }
+            else
+            {
+                Console.WriteLine("The index {0} is out of range for the string list", listIndex2);
+            }
+        }
+    }
+}
---
+++ StringAssignmentSubmission/StringAssignmentSubmission/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace StringAssignmentSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Concatenate three strings
+            string str1 = "Hello";
+            string str2 = " ";
+            string str3 = "World";
+            string concatenatedString = str1 + str2 + str3;
+            Console.WriteLine(concatenatedString);
+
+            // Convert a string to uppercase
+            string lowercaseString = "this is a lowercase string";
+            string uppercaseString = lowercaseString.ToUpper();
+            Console.WriteLine(uppercaseString);
+
+            // Create a StringBuilder and build a paragraph of text, one sentence at a time
+            StringBuilder paragraphBuilder = new StringBuilder();
+            paragraphBuilder.Append("The quick brown fox ");
+            paragraphBuilder.Append("jumps over the lazy dog. ");
+            paragraphBuilder.Append("The quick brown fox ");
+            paragraphBuilder.Append("jumps over the lazy dog again. ");
+            string paragraph = paragraphBuilder.ToString();
+            Console.WriteLine(paragraph);
+        }
+    }
+}
