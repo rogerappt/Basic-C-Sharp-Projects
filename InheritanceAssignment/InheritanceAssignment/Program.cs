ca6a8ca8534904cfdc62cd249816a90d957a9e7a
rogerappt <marius.preda85@gmail.com>
2023-02-23 12:41:39 +0000

    Inheritance Assignment

---
+++ InheritanceAssignment/InheritanceAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public void SayName()
+    {
+        Console.WriteLine($"Name: {FirstName} {LastName}");
+    }
+}
+
+public class Employee : Person
+{
+    public int Id { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee employee = new Employee
+        {
+            FirstName = "Sample",
+            LastName = "Student",
+            Id = 1234
+        };
+
+        employee.SayName();
+    }
+}
