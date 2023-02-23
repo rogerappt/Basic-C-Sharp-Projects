405abf1faf2b609ac467dd67d2fae9327abaa72c
rogerappt <marius.preda85@gmail.com>
2023-02-23 12:59:29 +0000

    Abstract Classes Assignment

---
+++ AbstractClassesAssignment/AbstractClassesAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PersonExample
+{
+    abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public abstract void SayName();
+    }
+
+    class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine($"My name is {firstName} {lastName}.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee()
+            {
+                firstName = "Sample",
+                lastName = "Student"
+            };
+
+            employee.SayName();
+
+            Console.ReadKey();
+        }
+    }
+}
