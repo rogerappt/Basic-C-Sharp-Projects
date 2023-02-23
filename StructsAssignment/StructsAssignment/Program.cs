b9f8496a0be3a3fa52ee77d9eccd5edf2bd47aeb
rogerappt <marius.preda85@gmail.com>
2023-02-23 13:26:11 +0000

    Structs Assignment

---
+++ StructsAssignment/StructsAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+struct Number
+{
+    public decimal Amount { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Number myNumber = new Number { Amount = 123.45M };
+        Console.WriteLine("Amount: {0}", myNumber.Amount);
+        Console.ReadKey();
+    }
+}
