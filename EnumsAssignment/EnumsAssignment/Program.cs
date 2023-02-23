3b46c917a25ad0e578a4802ec02c4fa2bffe15e8
rogerappt <marius.preda85@gmail.com>
2023-02-23 13:22:44 +0000

    Enums Assignment

---
+++ EnumsAssignment/EnumsAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        enum DaysOfWeek { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the current day of the week:");
+                string userInput = Console.ReadLine();
+
+                DaysOfWeek currentDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), userInput);
+
+                Console.WriteLine("Today is " + currentDay.ToString());
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+        }
+    }
+}
