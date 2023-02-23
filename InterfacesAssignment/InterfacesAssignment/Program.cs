aae47590960fb668dd70b5852e8fd8cbffe28d7f
rogerappt <marius.preda85@gmail.com>
2023-02-23 13:03:41 +0000

    Interfaces Assignment

---
+++ InterfacesAssignment/InterfacesAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    interface IQuittable
+    {
+        void Quit();
+    }
+
+    class Employee : IQuittable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EmployeeId { get; set; }
+
+        // Implementation of the Quit method from the IQuittable interface
+        public void Quit()
+        {
+            Console.WriteLine($"{FirstName} {LastName} with Employee ID {EmployeeId} has quit.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of type Employee and call the Quit method on it
+            Employee employee = new Employee()
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                EmployeeId = 12345
+            };
+            employee.Quit();
+
+            // Create an object of type IQuittable and call the Quit method on it using polymorphism
+            IQuittable quittable = new Employee()
+            {
+                FirstName = "Jane",
+                LastName = "Doe",
+                EmployeeId = 67890
+            };
+            quittable.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
