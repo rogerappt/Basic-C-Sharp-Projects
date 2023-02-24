46df25611f2d9d815132bdd909b790d634053e17
rogerappt <marius.preda85@gmail.com>
2023-02-24 17:19:25 +0000

    Datetime Assignment

---
+++ DatetimeAssignment/DatetimeAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1: Print current date and time to console
+            Console.WriteLine("Current date and time: " + DateTime.Now);
+
+            // Step 2: Ask user for a number
+            Console.Write("Enter a number: ");
+            int hours = int.Parse(Console.ReadLine());
+
+            // Step 3: Calculate and print time after X hours
+            DateTime futureTime = DateTime.Now.AddHours(hours);
+            Console.WriteLine("The time after " + hours + " hours will be: " + futureTime);
+
+            // Wait for user to press any key before closing console
+            Console.ReadKey();
+        }
+    }
+}
