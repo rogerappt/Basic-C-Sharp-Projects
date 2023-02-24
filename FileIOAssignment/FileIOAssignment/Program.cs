f007c6dcdd22bd51487061d701cb2b5607b91eb6
rogerappt <marius.preda85@gmail.com>
2023-02-24 17:13:02 +0000

    File I/O Assignment

---
+++ FileIOAssignment/FileIOAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace NumberLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for a number
+            Console.Write("Enter a number: ");
+            string number = Console.ReadLine();
+
+            // Log the number to a text file
+            string filePath = "numbers.txt";
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine(number);
+            }
+
+            // Print the contents of the text file to the console
+            Console.WriteLine("Contents of numbers.txt:");
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
